fc852ea7d8c4d276c09d9863798afb56300e1b8a
Carlos Gavidia-Calderon <cgavidia-calderon@turing.ac.uk>
2024-05-10 11:15:22 +0100

    Moving code to the main method

--- titanic.py
+++ titanic.py
@@ -3,9 +3,20 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 file_name: str = "titanic3.csv"
-data_frame: pd.DataFrame = pd.read_csv(file_name)
 
-figure, axis = plt.subplots(ncols=2, figsize=(30, 5))
-sns.violinplot(x="survived", y="Age", hue="sex", data=data_frame, ax=axis[0])
-sns.violinplot(x="survived", y="Fare", hue="sex", data=data_frame, ax=axis[1])
-plt.show()
+
+def main() -> None:
+    data_frame: pd.DataFrame = pd.read_csv(file_name)
+
+    figure, axis = plt.subplots(ncols=2, figsize=(30, 5))
+    sns.violinplot(
+        x="survived", y="age", hue="sex", data=data_frame, ax=axis[0]
+    )
+    sns.violinplot(
+        x="survived", y="fare", hue="sex", data=data_frame, ax=axis[1]
+    )
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
